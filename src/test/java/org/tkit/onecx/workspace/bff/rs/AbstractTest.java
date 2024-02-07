9d1e6c570e805899c4c1488904d18bb809df2a11
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-02-07 15:42:58 +0100

    fix: removed unused code

--- src/test/java/org/tkit/onecx/workspace/bff/rs/AbstractTest.java
+++ src/test/java/org/tkit/onecx/workspace/bff/rs/AbstractTest.java
@@ -25,9 +25,6 @@ public abstract class AbstractTest {
     protected static final String APM_HEADER_PARAM = ConfigProvider.getConfig()
             .getValue("%test.tkit.rs.context.token.header-param", String.class);
 
-    //    protected static final String CLAIMS_ORG_ID = ConfigProvider.getConfig()
-    //            .getValue("%test.tkit.rs.context.tenant-id.mock.claim-org-id", String.class);
-
     static {
         RestAssured.config = RestAssuredConfig.config().objectMapperConfig(
                 ObjectMapperConfig.objectMapperConfig().jackson2ObjectMapperFactory(
@@ -39,18 +36,4 @@ public abstract class AbstractTest {
                         }));
     }
 
-    //    protected static String createToken(String userId, String orgId) {
-    //        try {
-    //            String userName = userId != null ? userId : "test-user";
-    //            String organizationId = orgId != null ? orgId : "org1";
-    //            JsonObjectBuilder claims = Json.createObjectBuilder();
-    //            claims.add(Claims.preferred_username.name(), userName);
-    //            claims.add(Claims.sub.name(), userName);
-    //            claims.add(CLAIMS_ORG_ID, organizationId);
-    //            PrivateKey privateKey = KeyUtils.generateKeyPair(2048).getPrivate();
-    //            return Jwt.claims(claims.build()).sign(privateKey);
-    //        } catch (Exception ex) {
-    //            throw new RuntimeException(ex);
-    //        }
-    //    }
 }
