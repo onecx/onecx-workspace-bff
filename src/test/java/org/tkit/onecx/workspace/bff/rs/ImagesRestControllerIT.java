7c8416e4c72a64033dfcfca951fb58973dcab3a1
Jannik Steenken <jannik.steenken@capgemini.com>
2024-02-29 14:16:07 +0100

    fix: format

--- src/test/java/org/tkit/onecx/workspace/bff/rs/ImagesRestControllerIT.java
+++ src/test/java/org/tkit/onecx/workspace/bff/rs/ImagesRestControllerIT.java
@@ -1,10 +1,8 @@
 package org.tkit.onecx.workspace.bff.rs;
 
-
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 public class ImagesRestControllerIT extends ImagesRestControllerTest {
 
 }
-
