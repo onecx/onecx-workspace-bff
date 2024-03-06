a871920da071a55828befbe781a8bbff8e5765e4
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-03-06 11:20:15 +0100

    feat: added iam api + endpoint + test

---
+++ src/main/java/org/tkit/onecx/workspace/bff/rs/mappers/RoleMapper.java
@@ -0,0 +1,17 @@
+package org.tkit.onecx.workspace.bff.rs.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import gen.org.tkit.onecx.iam.client.model.RolePageResult;
+import gen.org.tkit.onecx.iam.client.model.RoleSearchCriteria;
+import gen.org.tkit.onecx.workspace.bff.rs.internal.model.IAMRolePageResultDTO;
+import gen.org.tkit.onecx.workspace.bff.rs.internal.model.IAMRoleSearchCriteriaDTO;
+
+@Mapper
+public interface RoleMapper {
+    RoleSearchCriteria map(IAMRoleSearchCriteriaDTO searchCriteriaDTO);
+
+    @Mapping(target = "removeStreamItem", ignore = true)
+    IAMRolePageResultDTO map(RolePageResult rolePageResult);
+}
