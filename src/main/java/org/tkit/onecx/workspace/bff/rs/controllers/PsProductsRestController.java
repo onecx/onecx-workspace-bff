f991fccf9b016dbc722b915019c9823585af6858
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-03-27 13:07:23 +0100

    feat: added mfes to availableProducts endpoint (#44)

--- src/main/java/org/tkit/onecx/workspace/bff/rs/controllers/PsProductsRestController.java
+++ src/main/java/org/tkit/onecx/workspace/bff/rs/controllers/PsProductsRestController.java
@@ -7,7 +7,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.tkit.onecx.workspace.bff.rs.mappers.ProductMapper;
 
 import gen.org.tkit.onecx.product.store.client.api.ProductsApi;
-import gen.org.tkit.onecx.product.store.client.model.ProductItemPageResult;
+import gen.org.tkit.onecx.product.store.client.model.ProductsLoadResult;
 import gen.org.tkit.onecx.workspace.bff.rs.internal.ProductsApiService;
 import gen.org.tkit.onecx.workspace.bff.rs.internal.model.ProductStorePageResultDTO;
 import gen.org.tkit.onecx.workspace.bff.rs.internal.model.ProductStoreSearchCriteriaDTO;
@@ -24,8 +24,8 @@ public class PsProductsRestController implements ProductsApiService {
     @Override
     public Response searchAvailableProducts(ProductStoreSearchCriteriaDTO productStoreSearchCriteriaDTO) {
         try (Response response = productStoreClient
-                .searchProductsByCriteria(productMapper.map(productStoreSearchCriteriaDTO))) {
-            ProductStorePageResultDTO availableProducts = productMapper.map(response.readEntity(ProductItemPageResult.class));
+                .loadProductsByCriteria(productMapper.map(productStoreSearchCriteriaDTO))) {
+            ProductStorePageResultDTO availableProducts = productMapper.map(response.readEntity(ProductsLoadResult.class));
             return Response.status(response.getStatus()).entity(availableProducts).build();
         }
     }
