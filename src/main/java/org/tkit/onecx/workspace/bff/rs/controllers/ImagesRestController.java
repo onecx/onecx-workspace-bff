c7464a47e673c0896b6670795dd05d27b2c7018c
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-05-14 14:51:47 +0200

    feat: product-store logos

--- src/main/java/org/tkit/onecx/workspace/bff/rs/controllers/ImagesRestController.java
+++ src/main/java/org/tkit/onecx/workspace/bff/rs/controllers/ImagesRestController.java
@@ -15,6 +15,8 @@ import org.tkit.onecx.workspace.bff.rs.mappers.ExceptionMapper;
 import org.tkit.onecx.workspace.bff.rs.mappers.ImagesMapper;
 import org.tkit.quarkus.log.cdi.LogService;
 
+import gen.org.tkit.onecx.product.store.client.api.ImagesApi;
+import gen.org.tkit.onecx.product.store.client.model.RefType;
 import gen.org.tkit.onecx.workspace.bff.rs.internal.ImagesInternalApiService;
 import gen.org.tkit.onecx.workspace.bff.rs.internal.model.ImageInfoDTO;
 import gen.org.tkit.onecx.workspace.bff.rs.internal.model.ProblemDetailResponseDTO;
@@ -40,10 +42,34 @@ public class ImagesRestController implements ImagesInternalApiService {
     @Inject
     HttpHeaders headers;
 
+    @Inject
+    @RestClient
+    ImagesApi productStoreImageClient;
+
     @Override
     public Response getImage(String refId, RefTypeDTO refType) {
         Response.ResponseBuilder responseBuilder;
         try (Response response = imageApi.getImage(refId, imageMapper.map(refType))) {
+            var contentType = response.getHeaderString(HttpHeaders.CONTENT_TYPE);
+            var contentLength = response.getHeaderString(HttpHeaders.CONTENT_LENGTH);
+            var body = response.readEntity(byte[].class);
+            if (contentType != null && body.length != 0) {
+                responseBuilder = Response.status(response.getStatus())
+                        .header(HttpHeaders.CONTENT_TYPE, contentType)
+                        .header(HttpHeaders.CONTENT_LENGTH, contentLength)
+                        .entity(body);
+            } else {
+                responseBuilder = Response.status(Response.Status.BAD_REQUEST);
+            }
+
+            return responseBuilder.build();
+        }
+    }
+
+    @Override
+    public Response getProductLogo(String productName) {
+        Response.ResponseBuilder responseBuilder;
+        try (Response response = productStoreImageClient.getImage(productName, RefType.LOGO)) {
             var contentType = response.getHeaderString(HttpHeaders.CONTENT_TYPE);
             var contentLength = response.getHeaderString(HttpHeaders.CONTENT_LENGTH);
             var body = response.readEntity(byte[].class);
