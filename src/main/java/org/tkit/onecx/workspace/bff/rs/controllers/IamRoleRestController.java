8eb36241e1b5b669ffbf5927b13f9c3ab36f1019
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-10-23 10:36:44 +0200

    feat: updated response status code (#120)

--- src/main/java/org/tkit/onecx/workspace/bff/rs/controllers/IamRoleRestController.java
+++ src/main/java/org/tkit/onecx/workspace/bff/rs/controllers/IamRoleRestController.java
@@ -43,7 +43,7 @@ public class IamRoleRestController implements RoleApiService {
     @Override
     public Response searchAvailableRoles(IAMRoleSearchCriteriaDTO searchCriteriaDTO) {
         if (!config.restClients().iam().enabled()) {
-            return Response.status(Response.Status.NOT_FOUND).build();
+            return Response.status(418).build();
         }
         try (Response response = iamClient.rolesSearchByCriteria(mapper.map(searchCriteriaDTO))) {
             IAMRolePageResultDTO responseDTO = mapper
