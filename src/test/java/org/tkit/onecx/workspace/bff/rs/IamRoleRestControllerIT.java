4d50b6f40087eda42e5613aa28634e113f9110bf
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2025-03-18 11:09:24 +0100

    feat: removed iam v1 api impl (#150)

--- src/test/java/org/tkit/onecx/workspace/bff/rs/IamRoleRestControllerIT.java
+++
@@ -1,7 +0,0 @@
-package org.tkit.onecx.workspace.bff.rs;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class IamRoleRestControllerIT extends IamRoleRestControllerTest {
-}
